e5b4c22899a087aaca13501098deddd9d39d58f8
Elvis Wei <ewei068@berkeley.edu>
2023-11-05 12:55:56 -0800

    basic apkg import

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-// Load the sql.js library
 import initSqlJs from 'sql.js';
 import fetch from 'node-fetch';
 import { AnkiCollection } from './classes/AnkiCollection';
+import * as zip from '@zip.js/zip.js';
 
 interface IOptions {
     sqlConfig?: Partial<EmscriptenModule>
@@ -31,4 +31,30 @@ export async function readFromUrl(url: string, options?: IOptions): Promise<Anki
         });
 
     return collection;
+}
+
+export async function readAnkiPackage(ankiPackage: Blob, options?: IOptions): Promise<AnkiCollection> {
+    const reader = new zip.ZipReader(new zip.BlobReader(ankiPackage));
+    return await reader.getEntries().then(async entries => {
+        const collectionEntry = entries.find(entry => entry.filename === 'collection.anki2');
+        if (collectionEntry?.getData == null) {
+            throw new Error('Could not find collection.anki2 in Anki package');
+        }
+
+        const collectionArrayBuffer = await collectionEntry.getData(new zip.Uint8ArrayWriter());
+        return await readAnkiCollection(new Uint8Array(collectionArrayBuffer), options);
+    });
+}
+
+export async function readAnkiPackageFromUrl(url: string, options?: IOptions): Promise<AnkiCollection> {
+    const reader = new zip.ZipReader(new zip.HttpReader(url));
+    return await reader.getEntries().then(async entries => {
+        const collectionEntry = entries.find(entry => entry.filename === 'collection.anki2');
+        if (collectionEntry?.getData == null) {
+            throw new Error('Could not find collection.anki2 in Anki package');
+        }
+
+        const collectionArrayBuffer = await collectionEntry.getData(new zip.Uint8ArrayWriter());
+        return await readAnkiCollection(new Uint8Array(collectionArrayBuffer), options);
+    });
 }
---
+++ story/apkgExample.js
@@ -0,0 +1,51 @@
+import 'web-streams-polyfill'; // Polyfill TransformStream globally
+import 'isomorphic-fetch'; // Polyfill fetch globally
+
+import { readAnkiPackageFromUrl, readAnkiPackage } from '../lib/src/index.js';
+
+readAnkiPackageFromUrl('http://127.0.0.1:8081/Amino_Acid_Flashcards.apkg')
+    .then(collection => {
+        console.log('Creation time:', collection.getCreationTime());
+        console.log('Modification time:', collection.getModificationTime());
+        console.log('Schema modification time:', collection.getSchemaModificationTime());
+        console.log('version:', collection.getVersion());
+        console.log('dconf:', collection.getDconf());
+        console.log('models:', collection.getModels());
+        console.log('tags:', collection.getTags());
+        console.log('config:', collection.getConfig());
+
+        for (const deck of Object.values(collection.getDecks())) {
+            console.log('Deck:', deck.getRawDeck());
+            console.log('Name:', deck.getName());
+            console.log('Description:', deck.getDescription());
+
+            for (const card of Object.values(deck.getCards()).slice(0, 10)) {
+                console.log('Card:', card.getRawCard());
+                console.log('Fields:', card.getFields());
+                //console.log('Front:', card.getFront());
+                //console.log('Back:', card.getBack());
+                //console.log('Note ID:', card.getNoteId());
+                console.log('Model ID:', card.getModelId());
+
+                for (const question of card.getQuestions()) {
+                    console.log('Question:', question.getQuestion().formattedString);
+                    console.log('Answer:', question.getAnswer().formattedString);
+                }
+            }
+        }
+
+        for (const [modelId, model] of Object.entries(collection.getModels())) {
+            console.log('Model:', modelId);
+            console.log('Name:', model.getName());
+            console.log('Fields:', model.getFields());
+            console.log('Templates:', model.getTemplates());
+            console.log('CSS:', model.getCss());
+        }
+    });
+
+fetch('http://127.0.0.1:8081/Amino_Acid_Flashcards.apkg')
+    .then(res => res.blob())
+    .then(buffer => readAnkiPackage(buffer))
+    .then(collection => {
+        console.log('Creation time:', collection.getCreationTime());
+    });
